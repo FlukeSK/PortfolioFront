d012d8fa00a942e4bf0d659f7c0b3bbd15e9b825
FlukeSK <146964113+FlukeSK@users.noreply.github.com>
2024-06-20 14:29:07 +0700

    EditPageGift

--- src/pages/Gift.jsx
+++ src/pages/Gift.jsx
@@ -1,46 +1,43 @@
 export default function Gift() {
   return (
     <div
-    className=""
-    style={{
-      backgroundSize: "2000px 2000px",
-      backgroundImage:
-        "url(https://img.freepik.com/free-vector/blurred-valentine-s-day-wallpaper_23-2148819570.jpg?w=996&t=st=1710775724~exp=1710776324~hmac=5d64db4d8645a2be4c6bfeedb15cc1b7fd02d444c9822719a2493273ada71e80)",
-    }}
-  >
-      <div className="flex flex-col w-full pl-20 pr-20">
+      className=""
+      style={{
+        backgroundSize: "2000px 2000px",
+        backgroundImage:
+          "url(https://img.freepik.com/free-vector/blurred-valentine-s-day-wallpaper_23-2148819570.jpg?w=996&t=st=1710775724~exp=1710776324~hmac=5d64db4d8645a2be4c6bfeedb15cc1b7fd02d444c9822719a2493273ada71e80)",
+      }}
+    >
+      <div className="flex flex-col w-full pl-40 pr-40">
         <img
-          className=" object-fill"
-          src="https://www.parentsone.com/wp-content/uploads/2017/07/caturday-cat-cafe-shop-2.jpg"
-        />
-        <img
-          className=" object-fill"
-          src="https://d2e5ushqwiltxm.cloudfront.net/wp-content/uploads/sites/63/2016/11/10025010/destination-cat-cafe.jpg"
+          className="rounded-md"
+          src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t31.18172-8/10733732_376962332480363_5123144674951614829_o.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEMnkOEs8s8Ip3RmeVB56GfV78eR1-Fs0ZXvx5HX4WzRttfdbW25poC72OHpZUg9GTYWGTriz_iKeFhwLBm_sh5&_nc_ohc=Jcdl_ZD3qdcQ7kNvgH9klGD&_nc_ht=scontent.fbkk22-2.fna&oh=00_AYCQkG94nWfMAxwXo9t6i-uxqayLKI7mT-X_q5iUX5lg-g&oe=669B4680"
         />
       </div>
 
       <br />
-<div>
-<h1 className=" hover:scale-105 flex justify-center items-center font-bold text-gray-500 text-[2rem] p-4">
-        Mask
-      </h1>
-</div>
-      <div className="flex justify-around">
-        
-        {/* =============== Mask 01 =============== */}
+
+      <div className="flex justify-evenly">
+        {/* =============== Case 01 =============== */}
 
         <div className="flex justify-center rounded-md">
           <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10 bg-pink-200 rounded-tr-md rounded-tl-md p-10">
               <img
-                src="https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.6435-9/102958204_1623893784453872_6840606899478699033_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGNW5CkeU1eocuDRILveZ_02wbo0MJwg1rbBujQwnCDWkHMTC39xVmuTPTZ6yKpafB7p5dTYrBmllrEL4Dgn7b2&_nc_ohc=HTxdy_K_cVUAX8Not-h&_nc_ht=scontent.fbkk5-3.fna&oh=00_AfBXD7rtPBNXCKkjLtojd_geIbYfIf5Y3NsdxcVGz2JKLw&oe=662A98AE"
-                alt="Mask01"
-                className="rounded-xl"
+                src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t31.18172-8/10603999_376961985813731_3813238693302416714_o.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFHcnv8h5jWfyrK6QCGPLvr_4yShhhIPlr_jJKGGEg-WuyxUbmuk-4d4oofJsEv-ZlfM5EgouYLFTyDiS9r8eim&_nc_ohc=shGxwDAE3h8Q7kNvgH8Gw5l&_nc_ht=scontent.fbkk22-2.fna&oh=00_AYDa6nVUhWfAl4mVKzhc3U4vfZYIx0ELImfFQOKUfuZfsw&oe=669B4B0D"
+                alt="Case"
+                className="rounded-xl hover:scale-125"
               />
             </figure>
             <div className="card-body items-center text-center bg-white">
-              <h2 className="card-title font-bold p-[0.2rem]">Mask 01 Caturday</h2>
-              <p>Mask to protect against dust and cute patterns.</p>
+              <h2 className="card-title font-bold p-[0.2rem]">
+                Caturday Case Pink
+              </h2>
+              <p>
+                เคสมือถือพิมพ์รอบด้าน สำหรับ iPhone 5/5s <br />
+                และ Note 2/3 <br />
+                ราคา 450 บ.
+              </p>
               <div className="card-actions flex justify-center p-2">
                 <button className="bg-pink-400 rounded-md text-white hover:bg-pink-600 flex justify-center content-center border-2 border-pink-600 px-2">
                   Buy Now
@@ -50,20 +47,26 @@ export default function Gift() {
           </div>
         </div>
 
-        {/* =============== Mask 02 =============== */}
+        {/* =============== Case 02 =============== */}
 
         <div className="flex justify-center rounded-md">
           <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10 bg-pink-200 rounded-tr-md rounded-tl-md p-10">
               <img
-                src="https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.6435-9/103591660_1623893837787200_8091463697023877781_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFCT3iUYjZ49CFvlWzYgSXI9lUVnTF2DyX2VRWdMXYPJfrVeb7QHRmx6ayJRmCbFxgwBEMRz16ZSxEn01I3aTT7&_nc_ohc=IH3b_AJNhaEAX-qS4AY&_nc_oc=AdjrlG8AUwV_0hQE5EmALfKRiAyfIcg8pn-_eXV821FE0bHneWoqUfv7nyevceCSQLonsDmBg4Hy0E-coCyJT-af&_nc_ht=scontent.fbkk5-4.fna&oh=00_AfAWFm-_Idi1HZzKLNtsYMsYqnRoIAUUAQs6v3Xdya46RA&oe=662A968C"
-                alt="Mask02"
-                className="rounded-xl"
+                src="https://scontent.fbkk22-1.fna.fbcdn.net/v/t31.18172-8/1782459_376961982480398_4711677139092560926_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGWzG9amdee78yRgsCBTMSVkv3HSFl24ZKS_cdIWXbhkunoNDQAWz692bOb0IJN4K2BYdgCVmfoXEadqGHTtL3C&_nc_ohc=_lU7Y3Y2oK4Q7kNvgFgbh5l&_nc_ht=scontent.fbkk22-1.fna&oh=00_AYCY75ErovZeFsyKX_Jdnj7HN7Vx9Ney6YeEhP_CyWd8Dg&oe=669B5B13"
+                alt="Case"
+                className="rounded-xl hover:scale-125"
               />
             </figure>
             <div className="card-body items-center text-center bg-white">
-              <h2 className="card-title font-bold p-[0.2rem]">Mask 02 Caturday</h2>
-              <p>Mask to protect against dust and cute patterns.</p>
+              <h2 className="card-title font-bold p-[0.2rem]">
+                Caturday Case White
+              </h2>
+              <p>
+                รุ่น B&W Glow in the dark ได้ด้วยเมื่อยู่ในที่มืด <br />
+                เคสมือถือพิมพ์รอบด้าน สำหรับ iPhone 5/5s <br />
+                และ Note 2/3 ราคา 450 บ.
+              </p>
               <div className="card-actions flex justify-center p-2">
                 <button className="bg-pink-400 rounded-md text-white hover:bg-pink-600 flex justify-center content-center border-2 border-pink-600 px-2">
                   Buy Now
@@ -76,21 +79,27 @@ export default function Gift() {
 
       <br />
 
-      <div className="flex justify-around">
-        {/* =============== Mask 03 =============== */}
+      <div className="flex justify-evenly">
+        {/* =============== Case 03 =============== */}
 
         <div className="flex justify-center rounded-md">
           <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10 bg-pink-200 rounded-tr-md rounded-tl-md p-10">
               <img
-                src="https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.6435-9/103824806_1623893944453856_4434857371797383841_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFrroVgeBUBEDWttdgx4Yjr3LrqjLcWwUrcuuqMtxbBSrU4Abvp25gjxUvWvaF74op0PFGWDxRHKVsAbv3o11w-&_nc_ohc=BNs9dRmQdkUAX_xPwDr&_nc_ht=scontent.fbkk5-6.fna&oh=00_AfDRc2fv_I43tyhc4DeucOA7vLqzQN8_eWsXToZBlCpWCw&oe=662AC754"
-                alt="Mask03"
-                className="rounded-xl"
+                src="https://scontent.fbkk22-6.fna.fbcdn.net/v/t31.18172-8/10475926_376962069147056_4450119335918178117_o.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHCoVplm_5dUP3c92yXI8biaMoAxUPZP15oygDFQ9k_XhTachmghwjIaGDxVgM4pbAvYYbWgCrrGN9TraSBlCoY&_nc_ohc=DqP4U9buQasQ7kNvgHInpvP&_nc_ht=scontent.fbkk22-6.fna&oh=00_AYABFLxMhjZ58nc-5VZcDaIWG4Vf98x5xkYTChdTnxonrA&oe=669B2C33"
+                alt="Case"
+                className="rounded-xl hover:scale-125"
               />
             </figure>
             <div className="card-body items-center text-center bg-white">
-              <h2 className="card-title font-bold p-[0.2rem]">Mask 03 Caturday</h2>
-              <p>Mask to protect against dust and cute patterns.</p>
+              <h2 className="card-title font-bold p-[0.2rem]">
+                Caturday Case Green
+              </h2>
+              <p>
+                Caturday Case เคสมือถือพิมพ์รอบด้าน สำหรับ iPhone 5/5s และ Note
+                2/3 <br />
+                ราคา 450 บ.
+              </p>
               <div className="card-actions flex justify-center p-2">
                 <button className="bg-pink-400 rounded-md text-white hover:bg-pink-600 flex justify-center content-center border-2 border-pink-600 px-2">
                   Buy Now
@@ -100,20 +109,26 @@ export default function Gift() {
           </div>
         </div>
 
-        {/* =============== Mask 04 =============== */}
+        {/* =============== Case 04 =============== */}
 
         <div className="flex justify-center rounded-md">
           <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10 bg-pink-200 rounded-tr-md rounded-tl-md p-10">
               <img
-                src="https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.6435-9/83312695_1623893904453860_5610747455717330714_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE00tOiD0ZwYH5hRBGFCYqGDxBGWa4eKGMPEEZZrh4oY7haPYOUrqYVtesOnV5XIy4zu6MBN-V2I363x-jpZn2o&_nc_ohc=Lt_BxT9L_YYAX9BXeFg&_nc_oc=AdjUdtqxzGQ_xADXfSLvmc77PimotCx6dMIAexDxNfH13cGhTtkL4rCQb2uq7afP-ypZkNY4RryxYbxkZSTHjAtL&_nc_ht=scontent.fbkk5-6.fna&oh=00_AfCR31hgH_dwmVoOdbZtTs6wa2pvFFtANDP4VNGQxG9E-Q&oe=662A9931"
-                alt="Mask04"
-                className="rounded-xl"
+                src="https://scontent.fbkk22-8.fna.fbcdn.net/v/t31.18172-8/10710500_376962542480342_6820901544751984434_o.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG5RsDmD2qRcEyvuNJbw754kq0KruFaTT6SrQqu4VpNPqz-Mnw2lX6eDaci6UhMv1KOBjOJWluTmMl1EzG_G6-Q&_nc_ohc=NnKOYUTCb4oQ7kNvgF66Vpb&_nc_ht=scontent.fbkk22-8.fna&oh=00_AYAKAsuAq3X0ygvrRvD84tGxjcplJZnRAYzZy5R0E3Ib2g&oe=669B36B2"
+                alt="Case"
+                className="rounded-xl hover:scale-125"
               />
             </figure>
             <div className="card-body items-center text-center bg-white">
-              <h2 className="card-title font-bold p-[0.2rem]">Mask 04 Caturday</h2>
-              <p>Mask to protect against dust and cute patterns.</p>
+              <h2 className="card-title font-bold p-[0.2rem]">
+                Caturday Case Yellow
+              </h2>
+              <p>
+                Caturday Case เคสมือถือพิมพ์รอบด้าน สำหรับ iPhone 5/5s และ Note
+                2/3 <br />
+                ราคา 450 บ.
+              </p>
               <div className="card-actions flex justify-center p-2">
                 <button className="bg-pink-400 rounded-md text-white hover:bg-pink-600 flex justify-center content-center border-2 border-pink-600 px-2">
                   Buy Now
@@ -122,8 +137,52 @@ export default function Gift() {
             </div>
           </div>
         </div>
-
       </div>
+
+<br />
+
+      <div className="flex flex-col w-full pl-40 pr-40">
+        <img
+          className="rounded-md justify-center"
+          src="https://scontent.fbkk22-6.fna.fbcdn.net/v/t31.18172-8/10655355_376977585812171_4370979464586703703_o.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFs29lxn-d3VxpM-X4PkOjrzXqXDBj5aJLNepcMGPlokhkf62MTmPShyQD1HmG0CkevNMVv0iZPKBziphsHrybj&_nc_ohc=A-8l8P47b84Q7kNvgHdo1Sr&_nc_ht=scontent.fbkk22-6.fna&oh=00_AYAsE8wADD002kqJycjx28zXQn0S6zRMHCF0BHisVw0wTw&oe=669B4423"
+        />
+      </div>
+
+<br />
+
+<div className="flex justify-evenly">
+        {/* =============== Coaster =============== */}
+
+        <div className="flex justify-center rounded-md">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10 bg-pink-200 rounded-tr-md rounded-tl-md p-10">
+              <img
+                src=
+                "https://scontent.fbkk22-8.fna.fbcdn.net/v/t31.18172-8/10648955_376977579145505_2208014978746344693_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFnodDiAtVXqJDWqRrmENV7PlG1dnDYU-I-UbV2cNhT4hvpvKlHw9AsJ4AdCAC3UcyLL36Gom7JGWjUEfFPYFCX&_nc_ohc=M2cU46z0cOwQ7kNvgHNREZU&_nc_ht=scontent.fbkk22-8.fna&oh=00_AYCAWJcKvnfmXl1sdhcTPxBgi0OYnnI9ynCgNdLhoot2lw&oe=669B4813"
+                alt="Coaster"
+                className="rounded-xl hover:scale-125"
+              />
+            </figure>
+            <div className="card-body items-center text-center bg-white">
+              <h2 className="card-title font-bold p-[0.2rem]">
+              จานรองแก้วสีแดง
+              </h2>
+              <p>
+                จานรองแก้ว แบบ Full Set Coaster CollectEM all <br />
+                ราคา 790 บ.
+              </p>
+              <div className="card-actions flex justify-center p-2">
+                <button className="bg-pink-400 rounded-md text-white hover:bg-pink-600 flex justify-center content-center border-2 border-pink-600 px-2">
+                  Buy Now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div >
+
+<br />
+
     </div>
   );
 }
